ab299606560da782f5d2bd44e516abe1175b1562
Maheer Ashhab Naveen <maheer.a.naveen@gmail.com>
2025-07-19 02:55:54 +0600

    created the Finder page, yet to make it responsive tmrw and start on the third page

--- src/finder.jsx
+++ src/finder.jsx
@@ -1,15 +1,43 @@
 import "./index.css";
-import NavBar from "./components/NavBar";
-import {gsap} from "gsap";
-import {useGSAP} from "@gsap/react";
+import gameFind from "./assets/gameFind.jpg";
+import turFind from "./assets/turFind.jpg";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(useGSAP);
 
 export default function FinderPage() {
   return (
-    <div className="flex justify-center w-screen h-screen">
-      <div>
-        <NavBar />
+    <div className="flex flex-col items-center justify-center w-screen h-screen -mt-30">
+      <div
+        className="relative bg-darkgreen w-57 h-65 rounded-xl mb-10 mr-20 -rotate-7
+                   flex items-center justify-center overflow-hidden">
+        <img
+          src={turFind}
+          alt="turFind"
+          className="w-full h-full object-cover rounded-xl opacity-80"
+        />
+        <div
+          className="absolute inset-0 flex flex-col justify-end items-center pb-5
+                     font-polysans text-6xl text-beige">
+          <span>tur-</span>
+          <span>Find</span>
+        </div>
+      </div>
+      <div
+        className="relative bg-green w-57 h-65 rounded-xl ml-20 rotate-7
+                   flex items-center justify-center overflow-hidden">
+        <img
+          src={gameFind}
+          alt="gameFind"
+          className="w-full h-full object-cover rounded-xl opacity-80"
+        />
+        <div
+          className="absolute inset-0 flex flex-col justify-end items-center pb-5
+                     font-polysans text-6xl text-beige">
+          <span>game-</span>
+          <span>Find</span>
+        </div>
       </div>
     </div>
   );
