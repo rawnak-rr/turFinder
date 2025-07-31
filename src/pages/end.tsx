43b8e4ee6b01f7f0d7709de89d49f3d1f3bf097e
rawnak-rr <rawnakd11@gmail.com>
2025-07-31 23:22:37 +1000

    made end page responsive but still not happy with design

--- src/pages/end.tsx
+++ src/pages/end.tsx
@@ -4,80 +4,78 @@ import outarrow from "../assets/icons/arrow-outward.svg";
 export default function EndPage() {
   return (
     <div className="w-full min-h-screen flex flex-col bg-almostwhite">
-      <div
-        className="flex-grow flex flex-col justify-center items-center pt-15
-                   font-degular font-black text-9xl">
+      <div className="flex-grow flex flex-col justify-center items-center pt-14 font-degular font-black text-5xl sm:text-6xl md:text-8xl lg:text-9xl leading-none">
         <span>LAUNCHING</span>
         <span>FALL 2025!</span>
       </div>
-      <div
-        className="bg-darkgreen w-full h-1/2 rounded-t-[120px]
-                   flex flex-col justify-evenly items-center">
-        <div className="flex items-center bg-almostblack w-105 h-30 rounded-xl">
+
+      <div className="relative bg-darkgreen w-full py-12 md:py-20 rounded-t-[60px] sm:rounded-t-[90px] md:rounded-t-[120px] flex flex-col items-center gap-10">
+        {/* Brand */}
+        <div className="flex items-center bg-almostblack w-72 sm:w-96 md:w-[420px] h-20 sm:h-24 md:h-30 rounded-xl">
           <img
             src={logo}
-            className="w-40 -ml-5"
+            className="w-28 sm:w-32 md:w-40 -ml-4 sm:-ml-5"
           />
-          <span className="font-helvetica font-black text-6xl text-almostwhite -ml-5">
+          <span className="font-helvetica font-black text-4xl sm:text-5xl md:text-6xl text-almostwhite -ml-4 sm:-ml-5">
             turFinder*
           </span>
         </div>
-        <div
-          className="flex flex-col mt-10 mb-5 items-center
-                     font-redhatmono text-sm text-white">
-          <span>sign up for EARLY ACCESS!</span>
-          <div>
+
+        <div className="flex flex-col items-center font-redhatmono text-xs sm:text-sm text-white">
+          <span className="uppercase tracking-wide">
+            sign up for early access!
+          </span>
+          <form
+            onSubmit={(e) => e.preventDefault()}
+            className="flex flex-col sm:flex-row mt-4 gap-3">
             <input
               type="email"
               placeholder="email"
-              className="bg-beige h-10 w-60 mt-3 p-4 mr-2 rounded-xl text-black placeholder:text-almostblack/60"
+              className="bg-beige h-10 w-60 p-4 rounded-xl text-black placeholder:text-almostblack/60"
             />
             <button
-              className="bg-yellow h-10 w-20 rounded-xl
-                         text-black font-helvetica font-bold
-                         cursor-pointer hover:bg-yellow/90 active:bg-yellow/70">
+              type="submit"
+              className="bg-yellow h-10 w-full sm:w-24 rounded-xl text-black font-helvetica font-bold hover:bg-yellow/90 active:bg-yellow/70">
               submit
             </button>
-          </div>
+          </form>
         </div>
-        <div
-          className="flex flex-col absolute left-20 gap-2 pt-10 cursor-pointer
-                     font-polysans font-bold text-3xl text-lightgreen">
-          <span className="flex flex-row gap-1 group hover:scale-110 active:opacity-60 duration-200">
+
+        <div className="flex flex-col gap-2 pt-8 font-polysans font-bold text-2xl sm:text-3xl text-lightgreen items-center md:items-start md:absolute md:left-20 md:top-10">
+          <div className="flex items-center gap-1 group hover:scale-110 active:opacity-60 duration-200 cursor-pointer">
             Home
-          </span>
-          <span className="flex flex-row gap-1 group hover:scale-110 active:opacity-60 duration-200">
+          </div>
+          <div className="flex items-center gap-1 group hover:scale-110 active:opacity-60 duration-200 cursor-pointer">
             Instagram
             <img
               src={outarrow}
-              className="w-8 -mb-1 group-hover:rotate-45 duration-200"
+              className="w-6 sm:w-7 md:w-8 -mb-1 group-hover:rotate-45 duration-200"
             />
-          </span>
-          <span className="flex flex-row gap-1 group hover:scale-110 active:opacity-60 duration-200">
+          </div>
+          <div className="flex items-center gap-1 group hover:scale-110 active:opacity-60 duration-200 cursor-pointer">
             Facebook
             <img
               src={outarrow}
-              className="w-8 -mb-1 group-hover:rotate-45 duration-200"
+              className="w-6 sm:w-7 md:w-8 -mb-1 group-hover:rotate-45 duration-200"
             />
-          </span>
-          <span className="flex flex-row gap-1 group hover:scale-110 active:opacity-60 duration-200">
+          </div>
+          <div className="flex items-center gap-1 group hover:scale-110 active:opacity-60 duration-200 cursor-pointer">
             Contact Us
-          </span>
+          </div>
         </div>
-        <div
-          className="flex flex-col absolute right-20 pt-5 gap-5 cursor-pointer
-                     font-unbounded font-normal text-xl text-left text-black">
-          <div className="flex flex-col">
-            <span>Rawnak</span>
-            <span>UNSW, Sydney</span>
+
+        <div className="flex flex-col gap-4 pt-8 font-unbounded text-base sm:text-lg text-black items-center md:items-start md:absolute md:right-20 md:top-10">
+          <div className="leading-none text-center md:text-left">
+            <span className="block font-semibold">Rawnak</span>
+            <span className="block">UNSW, Sydney</span>
           </div>
-          <div className="flex flex-col">
-            <span>Maheer</span>
-            <span>ASU, Tempe</span>
+          <div className="leading-none text-center md:text-left">
+            <span className="block font-semibold">Maheer</span>
+            <span className="block">ASU, Tempe</span>
           </div>
-          <div className="flex flex-col">
-            <span>Masroor</span>
-            <span>BUET, Dhaka</span>
+          <div className="leading-none text-center md:text-left">
+            <span className="block font-semibold">Masroor</span>
+            <span className="block">BUET, Dhaka</span>
           </div>
         </div>
       </div>
